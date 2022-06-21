fa9041a3eeda66c483948eb19c32aed220f3d10f
wolframhwang <hzw94@naver.com>
2022-06-22 01:21:59 +0900

    Initial Commit

---
+++ StockIWantGetToo/StockIWantGetTooApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockIWantGetTooApp.swift
+//  StockIWantGetToo
+//
+//  Created by 황지웅 on 2022/06/22.
+//
+
+import SwiftUI
+
+@main
+struct StockIWantGetTooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
